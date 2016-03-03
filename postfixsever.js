21540d4d1aa37cdb1c3a16764e23a7db304a671b
Vincent P. Minde <vincentminde@gmail.com>
2016-03-03 12:46:27 +0300

    Initial commit

---
+++ postfixsever.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Vincent P. Minde on 3/2/16.
+ */
+
+var email   = require("emailjs");
+
+postfixSend = function postfixSend(emailInfo, callback) {
+
+    var server  = email.server.connect({
+        user:    "vincentminde",
+        password: "Ilovemymum",
+        host:    "vincent-Dell-Precision-M3800",
+        ssl:     false
+    });
+
+    server.send({
+        text:    emailInfo.msg,
+        from:    emailInfo.from,
+        to:      emailInfo.to,
+        subject: emailInfo.subject,
+        //attachment:emailInfo.attachment
+
+    }, function(err, message) {
+        callback(err);
+    });
+
+}
+
+exports.postfixSend = postfixSend;
