e73d4aa943bf7ed708c47b1cc3b8c4f0ec17c4c9
Vincent P. Minde <vincentminde@gmail.com>
2016-06-13 14:54:25 +0300

    Made changes

--- start.js
+++ start.js
@@ -344,8 +344,8 @@ getUser().then(function(users){
     //Generate reports in batches
     generateReportsInBatch(pendingOrgUnits.slice(0,batchProcessNumber));
 
-    //Start an email thread which checks for users whose reports have been generated
-    interval = setInterval(emailThreadCallback, 10000);
+    //Start an email thread which checks for users whose reports have been generated after every 5 minutes
+    interval = setInterval(emailThreadCallback, 300000);
 },function(){
     console.log("Error Fetching Users.")
 })
